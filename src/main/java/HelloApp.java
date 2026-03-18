7f12faed6d9b59c655265c87f4faee8dfc091582
os4023-beep <os4023@srmist.edu.in>
2026-03-18 14:52:50 +0530

    UC4 completed

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -7,19 +7,7 @@ public class HelloApp {
         if (args.length == 0) {
             name = "World";
         } else {
-
-            StringBuilder nameBuilder = new StringBuilder();
-            boolean first = true;
-
-            for (String n : args) {
-                if (!first) {
-                    nameBuilder.append(", ");
-                }
-                nameBuilder.append(n);
-                first = false;
-            }
-
-            name = nameBuilder.toString();
+            name = String.join(", ", args);
         }
 
         System.out.println("Hello, " + name + "!");
